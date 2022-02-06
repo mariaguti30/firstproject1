c4adafbf852fd61b6f300a6f3e8abb6a21b6b054
mariaguti30 <96922235+mariaguti30@users.noreply.github.com>
2022-02-06 04:30:45 +0000

    updated file

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 class HelloJava {
       public static void main (String[]{
-      System.out.println("Hello, I am Java your program!");
+      System.out.println("I am Java your program!");
       }
 }
 
